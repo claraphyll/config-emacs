60e247f15585905563beba00e9365e900c489f27
clr <public@complexvector.space>
2025-03-31 19:19:12 +0200

    move gc tuning entirely to early-init

--- early-init.el
+++ early-init.el
@@ -1,5 +1,5 @@
 ;; random perf hacks from https://emacs-lsp.github.io/lsp-mode/page/performance/
-(setq gc-cons-threshold 100000000)
+(setq gc-cons-threshold (* 1024 1024 128))
 (setq read-process-output-max (* 1024 1024)) ;; 1mb
 
 ;; Using elpaca
