b42dce13b5d1fdc7238b10efd09cc92cf8b3d9e4
clr <public@complexvector.space>
2025-04-01 03:28:51 +0200

    call mode with t for some reason

--- init.el
+++ init.el
@@ -60,7 +60,7 @@
 (use-package projectile :ensure t :config (projectile-mode t) (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 (use-package dape :ensure t)
 (use-package magit :ensure t :config (add-hook 'after-save-hook 'magit-after-save-refresh-status t))
-(use-package diff-hl :ensure t :after magit :config (global-diff-hl-mode) (diff-hl-flydiff-mode) (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
+(use-package diff-hl :ensure t :after magit :config (global-diff-hl-mode t) (diff-hl-flydiff-mode t) (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
 (use-package evil :ensure t
   :init
   (setq evil-want-keybinding nil)
