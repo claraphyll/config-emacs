1098609f3aa114c198af51781bf17cd2538a2852
clr <public@complexvector.space>
2025-05-05 22:34:21 +0200

    add lexical scoping to early init

--- early-init.el
+++ early-init.el
@@ -1,7 +1,8 @@
-;; random perf hacks from https://emacs-lsp.github.io/lsp-mode/page/performance/
+;; -*- lexical-binding: t; -*-
+
+;; random perf hacks from https://emacs-lsp.github.io/lsp-mode/page/performance/  
 (setq gc-cons-threshold (* 1024 1024 128))
 (setq read-process-output-max (* 1024 1024)) ;; 1mb
 
 ;; Using elpaca
 (setq package-enable-at-startup nil)
-
